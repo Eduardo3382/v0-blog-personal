409ec43651ec7fafee60bad257edf46fe350891c
Eduardo3382 <39868347+Eduardo3382@users.noreply.github.com>
2026-01-18 18:27:34 -0300

    Update photos.tsx

--- components/photos.tsx
+++ components/photos.tsx
@@ -5,14 +5,14 @@ const photos = [
     id: 1,
     src: "/photos/Farol_de_buenosaires.jpg",
     alt: "Farol de Buenos Aires",
-    caption: "Farol histórico en una caminata por Buenos Aires",
+    caption: "Impactado por este Farol histórico y esa maravillosa cúpula de Buenos Aires",
   },
   {
     id: 2,
     src: "/photos/La_biblia_y_el_calefon.png",
     alt: "La biblia y el calefón",
-    caption: "Un clásico porteño capturado en imagen",
-  },
+    caption: "La biblia y el calefón, Respiradero del 1900 con una c+amara de seguridad en medio del del caos de cables
+   },
 ];
 
 export function Photos() {
