460da1f5cdb8a2ba460a6c9d18e5a77295eaafbe
Edu3382736 <Edu3382736@hotmail.com>
2026-01-25 18:16:58 -0300

    feat: add dynamic Open Graph and Twitter metadata to blog posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -22,6 +22,27 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     return {
         title: `${post.title} | Eduardo de la Fuente`,
         description: post.excerpt,
+        openGraph: {
+            title: post.title,
+            description: post.excerpt,
+            type: "article",
+            publishedTime: post.date,
+            authors: ["Eduardo de la Fuente"],
+            images: post.coverImage ? [
+                {
+                    url: post.coverImage,
+                    width: 1200,
+                    height: 630,
+                    alt: post.title,
+                }
+            ] : [],
+        },
+        twitter: {
+            card: "summary_large_image",
+            title: post.title,
+            description: post.excerpt,
+            images: post.coverImage ? [post.coverImage] : [],
+        },
     };
 }
 
