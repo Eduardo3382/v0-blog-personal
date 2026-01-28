3ca19f56818454f2a29133986041c0cd67923dd4
Edu3382736 <Edu3382736@hotmail.com>
2026-01-28 18:44:02 -0300

    config: allow local dev origin

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  experimental: {
+    allowedDevOrigins: ["192.168.0.244", "localhost:3000"],
+  },
 }
 
 export default nextConfig
