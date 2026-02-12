b31f7d5e33872f21aa59598f2f996fb103b06bc9
Edu3382736 <Edu3382736@hotmail.com>
2026-02-12 20:32:11 -0300

    docs: actualizar frase del último boletín

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -13,7 +13,7 @@ export const siteConfig = {
     },
     latestNewsletter: {
         title: "Boletín 100",
-        description: "La aplicacion olvidada que puede salvarnos de más de un olvido.",
+        description: "La aplicación olvidada que nos saca de apuros cuando la memoria falla.",
         url: "https://eduardoescritos.notion.site/Bolet-n-100-Centenario-3033fc8867ff80e6b68ae7d47be39490?source=copy_link",
         image: "/newsletter/Boletin100_calendar.jpg",
         allNewslettersUrl: "https://eduardoescritos.notion.site/Pastillitas-de-tecnolog-a-2c96916a4f714d4f8e1a1ccf0f4f50cd?source=copy_link"
